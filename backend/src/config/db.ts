9558abe372ae83e7d919f8ae57c78838586b5b3d
SoulSndr <oskanianvictor@yahoo.com>
2026-01-07 08:40:21 +0100

    retrait des informations qui sont en dur

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,11 +7,11 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../../..", ".env") });
 
 const pool = new Pool({
-    user: process.env.DB_USER || process.env.POSTGRES_USER || 'user',
-    password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD || '1234',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 5432,
-    database: process.env.DB_NAME || process.env.POSTGRES_DB || 'carbotrack_db',
+    user: process.env.DB_USER || process.env.POSTGRES_USER,
+    password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME || process.env.POSTGRES_DB,
 });
 
 export const testConnection = async (): Promise<void> => {
